ae2da408fba4379244c0a8c972495730445e91aa
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-08-21 11:41:42 +0200

    Rewrite online asset caching store to incur less synchronous overhead
    This commit attempts to reshuffle `OnlineAssetCachingStore` itself in a
    way that maybe results in more acceptable performance with users who are
    I/O-capped.
    
    It is somewhat difficult for me to tell what the results will be in
    practice; in testing with a setup predicated on a slow pendrive, I
    appear to have basically wasted the pendrive to the degree that it looks
    almost completely fried to a degree that it was not when I started.
    
    That said, let's explain the changes.
    
    The entirety of the store to the realm file store occurs outside of the
    main operation
    -----------------------------------------------------------------------
    
    One thing that I didn't really consider when writing the original
    version of the cache is that in the event of a cache miss, the loading
    time of the asset is increased by the *totality* of the duration
    required to copy the asset out into the store. In scenarios of
    significant I/O degradation this may appear as significantly worsened
    UX.
    
    To counteract this, the store now implements `IResourceStore<byte>?`
    itself. In the event of a cache miss, the memory stream containing the
    retrieved online asset is copied out; the caller receives the original,
    while the copy is used to store the asset to disk and to realm entirely
    in the background.
    
    All writes are turned async
    ---------------------------
    
    The rationale for this is given mostly in the previous commit's
    description.
    
    As a side note, this links to the previous point.
    `RealmFileStore.Add()`, of particular note, can return an *already
    existing, managed* realm model. Trying tricks like
    
    ```csharp
    (var asset, path) = realmAccess.Run(r =>
    {
        var file = fileStore.Add(onlineStream, r, addToRealm: false);
        return (new RealmOnlineAsset(file, url), file.GetStoragePath());
    });
    realmAccess.WriteAsync(r => r.Add(asset)).ContinueWith(t =>
    {
        if (t.IsFaulted)
    	Logger.Log($@"Failed to write {nameof(RealmOnlineAsset)} to realm: {t.Exception}", LoggingTarget.Database);
    });
    ```
    
    is thus very unsafe because there's no real guarantee that nobody hasn't
    e.g. completely moved the realm between the read and the write, and thus
    exceptions of "objects belonging to closed realms" can surface.
    
    Separate url-to-local-path mapping is maintained
    ------------------------------------------------
    
    A dictionary is added which maps remote asset URLs to local file paths.
    This short-circuits realm in two ways: subsequent lookups of any single
    asset no longer hits realm synchronously for the realm, *and* subsequent
    lookups of any *already-cached* asset no longer updates the last access
    time, which means that any asset will have its access time bumped at
    most once per session, decreasing the number of writes and thus the bad
    overheads.

--- osu.Game/Graphics/OnlineAssetCachingStore.cs
+++ osu.Game/Graphics/OnlineAssetCachingStore.cs
@@ -2,11 +2,21 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
+using osu.Framework.Logging;
 using osu.Framework.Platform;
 using osu.Game.Database;
+using osu.Game.Extensions;
+using osu.Game.Models;
 using osu.Game.Online;
+using Realms;
 
 namespace osu.Game.Graphics
 {
@@ -24,66 +34,103 @@ namespace osu.Game.Graphics
     /// as it both makes the caching ineffective <b>AND</b> trashes the cache with entries that will never be used again.
     /// </para>
     /// </summary>
-    public class OnlineAssetCachingStore : IDisposable
+    public class OnlineAssetCachingStore : IResourceStore<byte[]?>
     {
-        // ReSharper disable NotAccessedField.Local
         private readonly RealmAccess realmAccess;
-        private readonly OnlineStore onlineStore;
-        private readonly RealmFileStore fileStore;
+        private readonly RealmFileStore realmFileStore;
+        private readonly OnlineStore onlineResourceStore;
+        private readonly StorageBackedResourceStore localResourceStore;
         private readonly LargeTextureStore largeTextureStore;
+
+        private readonly ConcurrentDictionary<string, string> cachedPaths = [];
 
         public OnlineAssetCachingStore(GameHost host, RealmAccess realmAccess)
         {
             this.realmAccess = realmAccess;
-            onlineStore = new TrustedDomainOnlineStore();
-            fileStore = new RealmFileStore(realmAccess, host.Storage);
-            // largeTextureStore = new LargeTextureStore(host.Renderer, host.CreateTextureLoaderStore(new StorageBackedResourceStore(fileStore.Storage)));
-            largeTextureStore = new LargeTextureStore(host.Renderer, host.CreateTextureLoaderStore(onlineStore));
+            realmFileStore = new RealmFileStore(realmAccess, host.Storage);
+            onlineResourceStore = new TrustedDomainOnlineStore();
+            localResourceStore = new StorageBackedResourceStore(realmFileStore.Storage);
+            largeTextureStore = new LargeTextureStore(host.Renderer, host.CreateTextureLoaderStore(this));
         }
 
-        public Texture? Get(string url)
+        public Texture? Get(string url) => largeTextureStore.Get(url);
+
+        #region IResourceStore<byte[]>
+
+        byte[] IResourceStore<byte[]?>.Get(string name) => throw new NotSupportedException();
+
+        Task<byte[]?> IResourceStore<byte[]?>.GetAsync(string name, CancellationToken cancellationToken) => throw new NotSupportedException();
+
+        /// <remarks>
+        /// The reason why this class implements <see cref="IResourceStore{T}"/> explicitly
+        /// instead of just wrapping <see cref="largeTextureStore"/>
+        /// is that in the cache miss case, it is not desirable to wait for realm operations to complete
+        /// as that will induce additional delays on devices with slow disk I/O.
+        /// To this end, the online asset is retrieved, then copied off;
+        /// the original requester of the asset gets the original online stream, while the realm store write happens in the background.
+        /// </remarks>
+        Stream? IResourceStore<byte[]?>.GetStream(string url)
         {
-            if (string.IsNullOrWhiteSpace(url))
+            if (string.IsNullOrEmpty(url))
                 return null;
 
-            return largeTextureStore.Get(url);
+            if (cachedPaths.TryGetValue(url, out string? cachedPath))
+                return localResourceStore.GetStream(cachedPath);
 
-            // TODO: logic below temporarily disabled as it causes unacceptable performance on devices with slow I/O due to realm abuse
-            // see https://github.com/ppy/osu/issues/38651#issuecomment-5356443643 for details
+            string? path = realmAccess.Run(r =>
+            {
+                var a = r.All<RealmOnlineAsset>().Filter($@"{nameof(RealmOnlineAsset.File)}.{nameof(RealmNamedFileUsage.Filename)} == $0", url).FirstOrDefault();
+                return a?.File.File.GetStoragePath();
+            });
 
-            // string? path = realmAccess.Write(r =>
-            // {
-            //     var a = r.All<RealmOnlineAsset>().Filter($@"{nameof(RealmOnlineAsset.File)}.{nameof(RealmNamedFileUsage.Filename)} == $0", url).FirstOrDefault();
-            //     if (a != null)
-            //         a.LastAccessed = DateTimeOffset.Now;
-            //     return a?.File.File.GetStoragePath();
-            // });
+            if (path == null)
+            {
+                var onlineStream = onlineResourceStore.GetStream(url);
 
-            // if (path == null)
-            // {
-            //     var onlineStream = onlineStore.GetStream(url);
+                if (onlineStream == null)
+                    return null;
 
-            //     if (onlineStream == null)
-            //         return null;
+                var copyForAsyncWrite = new MemoryStream();
+                onlineStream.CopyTo(copyForAsyncWrite);
+                // relies on the underlying stream being `MemoryStream` (implementation-dependent)
+                onlineStream.Seek(0, SeekOrigin.Begin);
 
-            //     path = realmAccess.Write(r =>
-            //     {
-            //         var file = fileStore.Add(onlineStream, r);
-            //         r.Add(new RealmOnlineAsset(file, url));
-            //         return file.GetStoragePath();
-            //     });
-            // }
-            // else
-            // {
-            //     Logger.Log($"Online asset {url} retrieved from {nameof(OnlineAssetCachingStore)}.", LoggingTarget.Network);
-            // }
+                realmAccess.WriteAsync(r =>
+                {
+                    var file = realmFileStore.Add(copyForAsyncWrite, r, addToRealm: false);
+                    return r.Add(new RealmOnlineAsset(file, url));
+                }).ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                        Logger.Log($@"Failed to write {nameof(RealmOnlineAsset)} to realm: {t.Exception}", LoggingTarget.Database);
+                });
 
-            // return largeTextureStore.Get(path);
+                return onlineStream;
+            }
+
+            Logger.Log($"Online asset {url} retrieved from {nameof(OnlineAssetCachingStore)}.", LoggingTarget.Network);
+            realmAccess.WriteAsync(r =>
+            {
+                var a = r.All<RealmOnlineAsset>().Filter($@"{nameof(RealmOnlineAsset.File)}.{nameof(RealmNamedFileUsage.Filename)} == $0", url).FirstOrDefault();
+                if (a != null)
+                    a.LastAccessed = DateTimeOffset.Now;
+            }).ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                    Logger.Log($@"Failed to update last access date of {nameof(RealmOnlineAsset)} to realm: {t.Exception}", LoggingTarget.Database);
+            });
+
+            cachedPaths.TryAdd(url, path);
+            return localResourceStore.GetStream(path);
         }
+
+        IEnumerable<string> IResourceStore<byte[]?>.GetAvailableResources() => throw new NotSupportedException();
+
+        #endregion
 
         public void Dispose()
         {
-            onlineStore.Dispose();
+            onlineResourceStore.Dispose();
             largeTextureStore.Dispose();
         }
     }

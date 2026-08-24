ppy/osu

commit d9c73e12adff2feaae4a3e158d36fe5883faf6ca

Inherit previous object's sample settings in polygon tool (#38579)
Closes https://github.com/ppy/osu/issues/30589.

To achieve this sample assignment logic is moved out from
`ComposeBlueprintContainer` and `HitObjectPlacementBlueprint` into a
unified `PlacementStateManager` (willing to negotiate naming) component.

---------

Co-authored-by: Dean Herbert <pe@ppy.sh>

osu.Game/Rulesets/Edit/HitObjectPlacementBlueprint.cs
@@ -12,9 +12,9 @@ using osu.Game.Audio;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Objects;
-using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Screens.Edit;
 using osu.Game.Screens.Edit.Compose;
+using osu.Game.Screens.Edit.Compose.Components;
 using osuTK;
 
 namespace osu.Game.Rulesets.Edit
@@ -47,12 +47,12 @@ namespace osu.Game.Rulesets.Edit
 
         private Bindable<double> startTimeBindable = null!;
 
-        private HitObject? getPreviousHitObject() => beatmap.HitObjects.TakeWhile(h => h.StartTime <= startTimeBindable.Value).LastOrDefault();
-
         protected override bool IsValidForPlacement => HitObject.StartTime >= beatmap.ControlPointInfo.TimingPoints.FirstOrDefault()?.Time;
 
         [Resolved]
         private IPlacementHandler placementHandler { get; set; } = null!;
+
+        private PlacementStateManager placementStateManager = null!;
 
         /// <summary>
         /// Acceptable leniency to account for rounding errors and minor unsnaps that we generally
@@ -80,6 +80,8 @@ namespace osu.Game.Rulesets.Edit
         [BackgroundDependencyLoader]
         private void load()
         {
+            AddInternal(placementStateManager = new PlacementStateManager(HitObject));
+
             startTimeBindable = HitObject.StartTimeBindable.GetBoundCopy();
             startTimeBindable.BindValueChanged(_ => ApplyDefaultsToHitObject(), true);
         }
@@ -119,45 +121,9 @@ namespace osu.Game.Rulesets.Edit
         public override SnapResult UpdateTimeAndPosition(Vector2 screenSpacePosition, double time)
         {
             if (PlacementActive == PlacementState.Waiting)
-            {
                 HitObject.StartTime = time;
 
-                if (HitObject is IHasComboInformation comboInformation)
-                    comboInformation.UpdateComboInformation(getPreviousHitObject() as IHasComboInformation);
-            }
-
-            var lastHitObject = getPreviousHitObject();
-            var lastHitNormal = lastHitObject?.Samples?.FirstOrDefault(o => o.Name == HitSampleInfo.HIT_NORMAL);
-
-            if (lastHitNormal != null && AutomaticBankAssignment)
-                // Inherit the bank from the previous hit object
-                HitObject.Samples = HitObject.Samples.Select(s => s.Name == HitSampleInfo.HIT_NORMAL ? s.With(newBank: lastHitNormal.Bank, newEditorAutoBank: true) : s).ToList();
-            else
-                HitObject.Samples = HitObject.Samples.Select(s => s.Name == HitSampleInfo.HIT_NORMAL ? s.With(newEditorAutoBank: false) : s).ToList();
-
-            if (lastHitNormal != null)
-            {
-                // Inherit the volume and sample set info from the previous hit object
-                HitObject.Samples = HitObject.Samples.Select(s => s.With(
-                    newVolume: lastHitNormal.Volume,
-                    newSuffix: lastHitNormal.Suffix,
-                    newUseBeatmapSamples: lastHitNormal.UseBeatmapSamples)).ToList();
-            }
-
-            if (AutomaticAdditionBankAssignment)
-            {
-                string bank = HitObject.Samples.FirstOrDefault(s => s.Name == HitSampleInfo.HIT_NORMAL)?.Bank ?? HitSampleInfo.BANK_SOFT;
-                HitObject.Samples = HitObject.Samples.Select(s => s.Name != HitSampleInfo.HIT_NORMAL ? s.With(newBank: bank, newEditorAutoBank: true) : s).ToList();
-            }
-            else
-                HitObject.Samples = HitObject.Samples.Select(s => s.Name != HitSampleInfo.HIT_NORMAL ? s.With(newEditorAutoBank: false) : s).ToList();
-
-            if (HitObject is IHasRepeats hasRepeats)
-            {
-                // Make sure all the node samples are identical to the hit object's samples
-                for (int i = 0; i < hasRepeats.NodeSamples.Count; i++)
-                    hasRepeats.NodeSamples[i] = HitObject.Samples.Select(o => o.With()).ToList();
-            }
+            placementStateManager.CopyStateFromPreviousObject();
 
             return new SnapResult(screenSpacePosition, time);
         }

osu.Game.Rulesets.Osu/Edit/PolygonGenerationPopover.cs
@@ -9,7 +9,6 @@ using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Beatmaps.ControlPoints;
-using osu.Game.Graphics.UserInterface;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Objects;
@@ -25,6 +24,8 @@ namespace osu.Game.Rulesets.Osu.Edit
 {
     public partial class PolygonGenerationPopover : OsuPopover
     {
+        private DependencyContainer dependencies = null!;
+
         private FormSliderBar<double> distanceSnapInput { get; set; } = null!;
         private FormSliderBar<int> offsetAngleInput { get; set; } = null!;
         private FormSliderBar<int> repeatCountInput { get; set; } = null!;
@@ -50,14 +51,16 @@ namespace osu.Game.Rulesets.Osu.Edit
         [Resolved]
         private HitObjectComposer composer { get; set; } = null!;
 
-        private Bindable<TernaryState> newComboState = null!;
+        private PlacementStateManager? placementStateManager;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            return dependencies = new DependencyContainer(parent);
+        }
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            var selectionHandler = (EditorSelectionHandler)composer.BlueprintContainer.SelectionHandler;
-            newComboState = selectionHandler.SelectionNewComboState.GetBoundCopy();
-
             AllowableAnchors = new[] { Anchor.CentreLeft, Anchor.CentreRight };
 
             Child = new FillFlowContainer
@@ -120,6 +123,10 @@ namespace osu.Game.Rulesets.Osu.Edit
                     }
                 }
             };
+
+            // TODO: This is a pretty ugly dependency chain.
+            // We should look to rearrange things to avoid this altogether.
+            dependencies.CacheAs(composer.BlueprintContainer.SelectionHandler);
         }
 
         protected override void LoadComplete()
@@ -133,7 +140,6 @@ namespace osu.Game.Rulesets.Osu.Edit
             offsetAngleInput.Current.BindValueChanged(_ => Scheduler.AddOnce(tryCreatePolygon));
             repeatCountInput.Current.BindValueChanged(_ => Scheduler.AddOnce(tryCreatePolygon));
             pointInput.Current.BindValueChanged(_ => Scheduler.AddOnce(tryCreatePolygon));
-            newComboState.BindValueChanged(_ => Scheduler.AddOnce(tryCreatePolygon));
             tryCreatePolygon();
         }
 
@@ -162,7 +168,6 @@ namespace osu.Game.Rulesets.Osu.Edit
             {
                 float angle = float.DegreesToRadians(offsetAngleInput.Current.Value) + (i + 1) * (2 * float.Pi / pointInput.Current.Value);
                 var position = OsuPlayfield.BASE_SIZE / 2 + new Vector2(polygonRadius * float.Cos(angle), polygonRadius * float.Sin(angle));
-                bool newCombo = i == 0 && newComboState.Value == TernaryState.True;
 
                 HitCircle circle;
 
@@ -172,9 +177,6 @@ namespace osu.Game.Rulesets.Osu.Edit
 
                     circle.Position = position;
                     circle.StartTime = startTime;
-                    circle.NewCombo = newCombo;
-
-                    editorBeatmap.Update(circle);
                 }
                 else
                 {
@@ -182,12 +184,10 @@ namespace osu.Game.Rulesets.Osu.Edit
                     {
                         Position = position,
                         StartTime = startTime,
-                        NewCombo = newCombo,
                     };
 
                     newlyAdded.Add(circle);
 
-                    // TODO: probably ensure samples also follow current ternary status (not trivial)
                     circle.Samples.Add(circle.CreateHitSampleInfo());
                 }
 
@@ -202,14 +202,14 @@ namespace osu.Game.Rulesets.Osu.Edit
                 startTime = beatSnapProvider.SnapTime(startTime + timeSpacing);
             }
 
-            var previousNewComboState = newComboState.Value;
-
             insertedCircles.AddRange(newlyAdded);
             editorBeatmap.AddRange(newlyAdded);
 
-            // When adding new hitObjects, newCombo state will get reset to false when no objects are selected.
-            // Since this is the case when this popover is showing, we need to restore the previous newCombo state
-            newComboState.Value = previousNewComboState;
+            placementStateManager?.RemoveAndDisposeImmediately();
+            Content.Add(placementStateManager = new PlacementStateManager(insertedCircles.Cast<HitObject>().ToArray())
+            {
+                PerformBeatmapUpdates = true,
+            });
 
             commitButton.Enabled.Value = true;
         }

osu.Game.Tests/Visual/Editing/TestSceneHitObjectSampleAdjustments.cs
@@ -1212,13 +1212,100 @@ namespace osu.Game.Tests.Visual.Editing
             hitObjectHasAutoAdditionBankFlag(0, true);
         }
 
-        private void clickSamplePiece(int objectIndex) => AddStep($"click {objectIndex.ToOrdinalWords()} sample piece", () =>
+        [Test]
+        public void TestPolygonGenerationInheritsSettings()
         {
-            var samplePiece = this.ChildrenOfType<SamplePointPiece>().Single(piece => piece is not NodeSamplePointPiece && piece.HitObject == EditorBeatmap.HitObjects.ElementAt(objectIndex));
+            AddStep("seek to 1000", () => EditorClock.Seek(1000));
+            AddStep("open polygon tool", () =>
+            {
+                InputManager.PressKey(Key.LShift);
+                InputManager.PressKey(Key.LControl);
+                InputManager.Key(Key.D);
+                InputManager.ReleaseKey(Key.LControl);
+                InputManager.ReleaseKey(Key.LShift);
+            });
 
-            InputManager.MoveMouseTo(samplePiece);
-            InputManager.Click(MouseButton.Left);
-        });
+            for (int i = 2; i <= 4; ++i)
+            {
+                hitObjectHasSamples(i, HitSampleInfo.HIT_NORMAL);
+                hitObjectHasSampleBank(i, HitSampleInfo.BANK_SOFT);
+                hitObjectHasAutoNormalBankFlag(i, true);
+                hitObjectHasSampleVolume(i, 60);
+            }
+
+            AddStep("add finish sound", () => InputManager.Key(Key.E));
+
+            for (int i = 2; i <= 4; ++i)
+            {
+                hitObjectHasSamples(i, HitSampleInfo.HIT_NORMAL, HitSampleInfo.HIT_FINISH);
+                hitObjectHasSampleBank(i, HitSampleInfo.BANK_SOFT);
+                hitObjectHasAutoNormalBankFlag(i, true);
+                hitObjectHasAutoAdditionBankFlag(i, true);
+                hitObjectHasSampleVolume(i, 60);
+            }
+
+            AddStep("set addition bank to drum", () =>
+            {
+                InputManager.PressKey(Key.LAlt);
+                InputManager.Key(Key.R);
+                InputManager.ReleaseKey(Key.LAlt);
+            });
+
+            for (int i = 2; i <= 4; ++i)
+            {
+                hitObjectHasSamples(i, HitSampleInfo.HIT_NORMAL, HitSampleInfo.HIT_FINISH);
+                hitObjectHasSampleNormalBank(i, HitSampleInfo.BANK_SOFT);
+                hitObjectHasAutoNormalBankFlag(i, true);
+                hitObjectHasSampleAdditionBank(i, HitSampleInfo.BANK_DRUM);
+                hitObjectHasAutoAdditionBankFlag(i, false);
+                hitObjectHasSampleVolume(i, 60);
+            }
+
+            AddStep("set normal bank to normal", () =>
+            {
+                InputManager.PressKey(Key.LShift);
+                InputManager.Key(Key.W);
+                InputManager.ReleaseKey(Key.LShift);
+            });
+
+            for (int i = 2; i <= 4; ++i)
+            {
+                hitObjectHasSamples(i, HitSampleInfo.HIT_NORMAL, HitSampleInfo.HIT_FINISH);
+                hitObjectHasSampleNormalBank(i, HitSampleInfo.BANK_NORMAL);
+                hitObjectHasAutoNormalBankFlag(i, false);
+                hitObjectHasSampleAdditionBank(i, HitSampleInfo.BANK_DRUM);
+                hitObjectHasAutoAdditionBankFlag(i, false);
+                hitObjectHasSampleVolume(i, 60);
+            }
+
+            AddStep("set addition bank to auto", () =>
+            {
+                InputManager.PressKey(Key.LAlt);
+                InputManager.Key(Key.Q);
+                InputManager.ReleaseKey(Key.LAlt);
+            });
+
+            for (int i = 2; i <= 4; ++i)
+            {
+                hitObjectHasSamples(i, HitSampleInfo.HIT_NORMAL, HitSampleInfo.HIT_FINISH);
+                hitObjectHasSampleBank(i, HitSampleInfo.BANK_NORMAL);
+                hitObjectHasAutoNormalBankFlag(i, false);
+                hitObjectHasAutoAdditionBankFlag(i, true);
+                hitObjectHasSampleVolume(i, 60);
+            }
+        }
+
+        private void clickSamplePiece(int objectIndex)
+        {
+            AddStep("switch tool to select", () => InputManager.Key(Key.Number1));
+            AddStep($"click {objectIndex.ToOrdinalWords()} sample piece", () =>
+            {
+                var samplePiece = this.ChildrenOfType<SamplePointPiece>().Single(piece => piece is not NodeSamplePointPiece && piece.HitObject == EditorBeatmap.HitObjects.ElementAt(objectIndex));
+
+                InputManager.MoveMouseTo(samplePiece);
+                InputManager.Click(MouseButton.Left);
+            });
+        }
 
         private void clickNodeSamplePiece(int objectIndex, int nodeIndex) => AddStep($"click {objectIndex.ToOrdinalWords()} object {nodeIndex.ToOrdinalWords()} node sample piece", () =>
         {

osu.Game/Screens/Edit/Compose/Components/ComposeBlueprintContainer.cs
@@ -22,7 +22,6 @@ using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Edit.Tools;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.UI;
 using osu.Game.Screens.Edit.Components.TernaryButtons;
 using osuTK;
@@ -34,9 +33,11 @@ namespace osu.Game.Screens.Edit.Compose.Components
     /// </summary>
     public abstract partial class ComposeBlueprintContainer : EditorBlueprintContainer
     {
+        private DependencyContainer dependencies = null!;
+
         private readonly Container<PlacementBlueprint> placementBlueprintContainer;
 
-        protected new EditorSelectionHandler SelectionHandler => (EditorSelectionHandler)base.SelectionHandler;
+        public new EditorSelectionHandler SelectionHandler => (EditorSelectionHandler)base.SelectionHandler;
 
         public PlacementBlueprint CurrentPlacement { get; private set; }
 
@@ -64,6 +65,11 @@ namespace osu.Game.Screens.Edit.Compose.Components
             };
         }
 
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            return dependencies = new DependencyContainer(parent);
+        }
+
         [BackgroundDependencyLoader]
         private void load()
         {
@@ -74,6 +80,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
             {
                 Child = placementBlueprintContainer
             });
+
+            dependencies.CacheAs(SelectionHandler);
         }
 
         protected override void LoadComplete()
@@ -85,19 +93,6 @@ namespace osu.Game.Screens.Edit.Compose.Components
             // updates to selected are handled for us by SelectionHandler.
             NewCombo.BindTo(SelectionHandler.SelectionNewComboState);
 
-            // we are responsible for current placement blueprint updated based on state changes.
-            NewCombo.ValueChanged += _ => updatePlacementNewCombo();
-
-            // we own SelectionHandler so don't need to worry about making bindable copies (for simplicity)
-            foreach (var kvp in SelectionHandler.SelectionSampleStates)
-                kvp.Value.BindValueChanged(_ => updatePlacementSamples());
-
-            foreach (var kvp in SelectionHandler.SelectionBankStates)
-                kvp.Value.BindValueChanged(_ => updatePlacementSamples());
-
-            foreach (var kvp in SelectionHandler.SelectionAdditionBankStates)
-                kvp.Value.BindValueChanged(_ => updatePlacementSamples());
-
             SelectionHandler.AutoSelectionBankEnabled.BindValueChanged(_ => updateAutoBankTernaryButtonTooltip(), true);
         }
 
@@ -107,68 +102,6 @@ namespace osu.Game.Screens.Edit.Compose.Components
 
             var blueprint = (HitObjectSelectionBlueprint)GetBlueprintFor(hitObject);
             blueprint.DrawableObject = drawableObject;
-        }
-
-        private void updatePlacementNewCombo()
-        {
-            if (CurrentHitObjectPlacement?.HitObject is IHasComboInformation c)
-                c.NewCombo = NewCombo.Value == TernaryState.True;
-        }
-
-        private void updatePlacementSamples()
-        {
-            if (CurrentHitObjectPlacement == null) return;
-
-            foreach (var kvp in SelectionHandler.SelectionSampleStates)
-                sampleChanged(kvp.Key, kvp.Value.Value);
-
-            foreach (var kvp in SelectionHandler.SelectionBankStates)
-                bankChanged(kvp.Key, kvp.Value.Value);
-
-            foreach (var kvp in SelectionHandler.SelectionAdditionBankStates)
-                additionBankChanged(kvp.Key, kvp.Value.Value);
-        }
-
-        private void sampleChanged(string sampleName, TernaryState state)
-        {
-            if (CurrentHitObjectPlacement == null) return;
-
-            var samples = CurrentHitObjectPlacement.HitObject.Samples;
-
-            var existingSample = samples.FirstOrDefault(s => s.Name == sampleName);
-
-            switch (state)
-            {
-                case TernaryState.False:
-                    if (existingSample != null)
-                        samples.Remove(existingSample);
-                    break;
-
-                case TernaryState.True:
-                    if (existingSample == null)
-                        samples.Add(CurrentHitObjectPlacement.HitObject.CreateHitSampleInfo(sampleName));
-                    break;
-            }
-        }
-
-        private void bankChanged(string bankName, TernaryState state)
-        {
-            if (CurrentHitObjectPlacement == null) return;
-
-            if (bankName == EditorSelectionHandler.HIT_BANK_AUTO)
-                CurrentHitObjectPlacement.AutomaticBankAssignment = state == TernaryState.True;
-            else if (state == TernaryState.True)
-                CurrentHitObjectPlacement.HitObject.Samples = CurrentHitObjectPlacement.HitObject.Samples.Select(s => s.Name == HitSampleInfo.HIT_NORMAL ? s.With(newBank: bankName) : s).ToList();
-        }
-
-        private void additionBankChanged(string bankName, TernaryState state)
-        {
-            if (CurrentHitObjectPlacement == null) return;
-
-            if (bankName == EditorSelectionHandler.HIT_BANK_AUTO)
-                CurrentHitObjectPlacement.AutomaticAdditionBankAssignment = state == TernaryState.True;
-            else if (state == TernaryState.True)
-                CurrentHitObjectPlacement.HitObject.Samples = CurrentHitObjectPlacement.HitObject.Samples.Select(s => s.Name != HitSampleInfo.HIT_NORMAL ? s.With(newBank: bankName) : s).ToList();
         }
 
         public readonly Bindable<TernaryState> NewCombo = new Bindable<TernaryState> { Description = "New Combo" };
@@ -403,10 +336,6 @@ namespace osu.Game.Screens.Edit.Compose.Components
 
                 // Fixes a 1-frame position discrepancy due to the first mouse move event happening in the next frame
                 updatePlacementTimeAndPosition();
-
-                updatePlacementSamples();
-
-                updatePlacementNewCombo();
             }
         }
 

osu.Game/Screens/Edit/Compose/Components/EditorSelectionHandler.cs
@@ -61,6 +61,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
 
         #region Selection State
 
+        public event Action? SelectionStateChanged;
+
         /// <summary>
         /// The state of "new combo" for all selected hitobjects.
         /// </summary>
@@ -150,6 +152,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
 
                             break;
                     }
+
+                    SelectionStateChanged?.Invoke();
                 };
 
                 SelectionBankStates[bankName] = bindable;
@@ -213,6 +217,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
 
                             break;
                     }
+
+                    SelectionStateChanged?.Invoke();
                 };
 
                 SelectionAdditionBankStates[bankName] = bindable;
@@ -239,6 +245,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
                             AddHitSample(sampleName);
                             break;
                     }
+
+                    SelectionStateChanged?.Invoke();
                 };
 
                 SelectionSampleStates[sampleName] = bindable;
@@ -257,6 +265,8 @@ namespace osu.Game.Screens.Edit.Compose.Components
                         SetNewCombo(true);
                         break;
                 }
+
+                SelectionStateChanged?.Invoke();
             };
         }
 

Rekiln/lazer

commit 5fac7d544521a72ed914d23e3b491fa1ab6500f8

Fix editor toggle states not applying to current placement
Reported internally.

Regressed in https://github.com/ppy/osu/pull/38579, though I will meekly
protest that the placement blueprint code was always dodgy because the
inheritance hierarchy here is `HitObjectPlacementBlueprint :
PlacementBlueprint : VisibilityContainer : Container`.

osu.Game.Rulesets.Catch/Edit/Blueprints/BananaShowerPlacementBlueprint.cs
@@ -23,7 +23,7 @@ namespace osu.Game.Rulesets.Catch.Edit.Blueprints
 
         public BananaShowerPlacementBlueprint()
         {
-            InternalChild = outline = new TimeSpanOutline();
+            Child = outline = new TimeSpanOutline();
         }
 
         protected override void LoadComplete()

osu.Game.Rulesets.Catch/Edit/Blueprints/FruitPlacementBlueprint.cs
@@ -16,7 +16,7 @@ namespace osu.Game.Rulesets.Catch.Edit.Blueprints
 
         public FruitPlacementBlueprint()
         {
-            InternalChild = outline = new FruitOutline();
+            Child = outline = new FruitOutline();
         }
 
         protected override void LoadComplete()

osu.Game.Rulesets.Catch/Edit/Blueprints/JuiceStreamPlacementBlueprint.cs
@@ -29,7 +29,7 @@ namespace osu.Game.Rulesets.Catch.Edit.Blueprints
 
         public JuiceStreamPlacementBlueprint()
         {
-            InternalChildren = new Drawable[]
+            Children = new Drawable[]
             {
                 scrollingPath = new ScrollingPath(),
                 nestedOutlineContainer = new NestedOutlineContainer(),

osu.Game.Rulesets.Mania/Edit/Blueprints/HoldNotePlacementBlueprint.cs
@@ -38,7 +38,7 @@ namespace osu.Game.Rulesets.Mania.Edit.Blueprints
         {
             RelativeSizeAxes = Axes.Both;
 
-            InternalChildren = new Drawable[]
+            Children = new Drawable[]
             {
                 bodyPiece = new EditBodyPiece { Origin = Anchor.TopCentre },
                 headPiece = new Circle

osu.Game.Rulesets.Mania/Edit/Blueprints/NotePlacementBlueprint.cs
@@ -28,7 +28,7 @@ namespace osu.Game.Rulesets.Mania.Edit.Blueprints
             RelativeSizeAxes = Axes.Both;
             Masking = true;
 
-            InternalChild = piece = new Circle
+            Child = piece = new Circle
             {
                 Origin = Anchor.Centre,
                 Colour = colours.Yellow,

osu.Game.Rulesets.Osu/Edit/Blueprints/HitCircles/HitCirclePlacementBlueprint.cs
@@ -31,7 +31,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.HitCircles
         public HitCirclePlacementBlueprint()
             : base(new HitCircle())
         {
-            InternalChild = circlePiece = new HitCirclePiece();
+            Child = circlePiece = new HitCirclePiece();
         }
 
         [BackgroundDependencyLoader]

osu.Game.Rulesets.Osu/Edit/Blueprints/Sliders/SliderPlacementBlueprint.cs
@@ -75,7 +75,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.Sliders
         [BackgroundDependencyLoader]
         private void load(OsuConfigManager config)
         {
-            InternalChildren = new Drawable[]
+            Children = new Drawable[]
             {
                 bodyPiece = new SliderBodyPiece(),
                 headCirclePiece = new HitCirclePiece(),

osu.Game.Rulesets.Osu/Edit/Blueprints/Spinners/SpinnerPlacementBlueprint.cs
@@ -27,7 +27,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.Spinners
         public SpinnerPlacementBlueprint()
             : base(new Spinner { Position = OsuPlayfield.BASE_SIZE / 2 })
         {
-            InternalChild = piece = new SpinnerPiece { Alpha = 0.5f };
+            Child = piece = new SpinnerPiece { Alpha = 0.5f };
         }
 
         protected override void Update()

osu.Game.Rulesets.Taiko/Edit/Blueprints/HitPlacementBlueprint.cs
@@ -23,7 +23,7 @@ namespace osu.Game.Rulesets.Taiko.Edit.Blueprints
         public HitPlacementBlueprint()
             : base(new Hit())
         {
-            InternalChild = piece = new HitPiece
+            Child = piece = new HitPiece
             {
                 Size = new Vector2(TaikoHitObject.DEFAULT_SIZE * TaikoPlayfield.BASE_HEIGHT)
             };

osu.Game.Rulesets.Taiko/Edit/Blueprints/TaikoSpanPlacementBlueprint.cs
@@ -37,7 +37,7 @@ namespace osu.Game.Rulesets.Taiko.Edit.Blueprints
 
             RelativeSizeAxes = Axes.Both;
 
-            InternalChildren = new Drawable[]
+            Children = new Drawable[]
             {
                 headPiece = new HitPiece
                 {

osu.Game/Rulesets/Edit/HitObjectPlacementBlueprint.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
 using System.Threading;
 using osu.Framework.Allocation;
@@ -31,6 +32,13 @@ namespace osu.Game.Rulesets.Edit
 
         [Resolved]
         protected EditorClock EditorClock { get; private set; } = null!;
+
+        protected override Container<Drawable> Content => content;
+
+        private readonly Container<Drawable> content = new Container<Drawable>
+        {
+            RelativeSizeAxes = Axes.Both,
+        };
 
         [Resolved]
         private EditorBeatmap beatmap { get; set; } = null!;
@@ -71,6 +79,7 @@ namespace osu.Game.Rulesets.Edit
         private void load()
         {
             AddInternal(placementStateManager = new PlacementStateManager(HitObject));
+            AddInternal(content);
 
             startTimeBindable = HitObject.StartTimeBindable.GetBoundCopy();
             startTimeBindable.BindValueChanged(_ => ApplyDefaultsToHitObject(), true);
@@ -137,5 +146,8 @@ namespace osu.Game.Rulesets.Edit
             base.PopOut();
             placementHandler.HidePlacement();
         }
+
+        protected override void ClearInternal(bool disposeChildren = true) =>
+            throw new InvalidOperationException($"Clearing {nameof(InternalChildren)} will cause critical failure. Use {nameof(Clear)} instead.");
     }
 }